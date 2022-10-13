83dade618df25a69186e243fd89089b8509fde0b
Musyoki <markmusyoki6@gmail.com>
2022-10-13 09:34:22 +0300

    Create index.js

---
+++ Blog-Card II/index.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cards">
+        <div class="card">
+            <img src="https://fakeimg.pl/150x150/009578/fff/" alt="" class="card__image">
+            <div class="card__content">
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto dicta quaerat impedit? Quibusdam perferendis quaerat mollitia, inventore sapiente ducimus laborum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto dicta
+                </p>
+            </div>
+            <div class="card__info">
+                <span>
+                    310
+                </span>
+                <div>
+                    <a href="" class="card__link">View Article</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
